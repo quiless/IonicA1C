ac4e0c8a954aa89c60bf16ebf6f9e6b6e9d97339
Kelvin Cleto <kcleto@outlook.com>
2018-07-20 20:05:58 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { MedicalResultsPage } from '../pages/medical-results/medical-results';
 import { HomePage } from '../pages/home/home';
 import { SplashPage } from '../pages/splash/splash';
 import { DashboardResultsPage } from '../pages/dashboard-results/dashboard-results';
+import { DashboardFilterComponent } from '../components/dashboard-filter/dashboard-filter';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = DashboardFilterComponent;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
     platform.ready().then(() => {
